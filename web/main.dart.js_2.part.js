5a6ba42eaf77eded614a3faf36e437f1ac73cdf0
Konzept <adnanmuratovic@gmail.com>
2022-06-18 20:15:14 +0200

    WRA

---
+++ web/main.dart.js_2.part.js
@@ -0,0 +1,23 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var A={
+a(d){return new A.bMK(d)},
+bY:function bY(){},
+bMK:function bMK(d){this.a=d},
+e0D(d,e,f){if(d!=="")return d
+return e}},B
+A=a.updateHolder(c[47],A)
+B=c[0]
+A.bY.prototype={
+bFF(d,e,f,g,h,i){var y=A.e0D(f,d,h),x=this.geQ().h(0,y)
+if(x==null)return d
+else return B.cVx(x,g,null)},
+h(d,e){return this.geQ().h(0,e)},
+j(d){return this.geX()}}
+var z=a.updateTypes([])
+A.bMK.prototype={
+$0(){return this.a},
+$S:20};(function inheritance(){var y=a.inherit
+y(A.bY,B.a9)
+y(A.bMK,B.amc)})()
+B.ez(b.typeUniverse,JSON.parse("{}"))}
+$__dart_deferred_initializers__["UbzxHYbPae0QgUB71IVjPB+qzgs="] = $__dart_deferred_initializers__.current
